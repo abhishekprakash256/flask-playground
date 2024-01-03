dbc487f7f6b10a2f3856150576be41bb3bca5a48
Abhishek <abhishekprakash47@gmail.com>
2024-01-03 15:44:16 -0500

    flask code

---
+++ testing/test2/app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def homepage():
+    """View function for Home Page."""
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """view the about page"""
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    """view the contact page"""
+    return render_template("contact.html")
+
+
+if __name__ == '__main__':  
+   app.run()
