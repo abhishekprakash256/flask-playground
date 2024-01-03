eb70e98cbfd14e2fbe1a784666db5c891d815555
Abhishek <abhishekprakash47@gmail.com>
2024-01-03 16:06:19 -0500

    flask code

--- testing/test2/app.py
+++ testing/test2/app.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route("/")
 def homepage():
     """View function for Home Page."""
     return render_template("index.html")
