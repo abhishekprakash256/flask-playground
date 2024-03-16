346d039cdb3d429235a4be675d13be165814f54a
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-16 00:57:33 -0400

    mongo files

--- testing/test14/mongo/mongo_db.py
+++ testing/test14/mongo/mongo_db.py
@@ -3,10 +3,10 @@ from pymongo import MongoClient
 # Connect to MongoDB
 client = MongoClient()
 
-# Get the list of databases
-db_list = client.list_database_names()
+# Create a new database
+db = client["articles"]
 
-# Print the list of databases
-print("List of databases:")
-for db in db_list:
-    print(db)
+
+# List the databases to verify
+print("List of databases after creating 'articles':")
+print(client.list_database_names())
