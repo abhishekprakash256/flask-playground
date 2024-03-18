e3597af48d46adab52e0d47365e76c0fd72eddf0
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-17 22:04:28 -0400

    mongo db

--- testing/test14/mongo/mongo_helper.py
+++ testing/test14/mongo/mongo_helper.py
@@ -21,10 +21,6 @@ def check_mongo_status():
         print("MongoDB is not installed.")
         return False
 
-# Usage example:
-
-
-
 
 #create the database client 
 def create_mongo_client():
@@ -36,7 +32,7 @@ def create_mongo_client():
         try:
 
             # Attempt to create a MongoClient
-            client = MongoClient("mongodb://localhost:27017/")
+            client = MongoClient('localhost', 27017)
             print("MongoDB client created successfully.")
             return client
 
@@ -62,37 +58,67 @@ mongo_client = create_mongo_client()
 #the helper class for the mongo functions 
 # Helper class for MongoDB functions
 class Helper_fun():
-    def make_database(self, db_name):
+    def __init__(self):
+        self.db = None
+        self.collection = None
+
+    def make_database_and_collection(self, db_name, db_collection):
         """
-        Make the database
+        Make the database and collection if they don't exist
         """
         # Print the list of existing databases before attempting to create the database
         print("Existing databases before creating '{}':".format(db_name), mongo_client.list_database_names())
 
         # Make the database if it doesn't exist
         if db_name not in mongo_client.list_database_names():
+            # Create the database
             self.db = mongo_client[db_name]
-            print("Database '{}' created.".format(db_name))
+
+            # Create the collection
+            self.collection = self.db[db_collection]
+
+            # Insert dummy data into the collection
+            dummy_data = {"dummy_data": True}
+            insert_data = self.collection.insert_one(dummy_data)
+
+            print("Database '{}' and collection '{}' created.".format(db_name, db_collection))
         else:
+            # If the database exists, select it
             self.db = mongo_client[db_name]
-            print("Database '{}' already exists.".format(db_name))
+            self.collection = self.db[db_collection]
+            print("Database '{}' and collection '{}' already exist.".format(db_name, db_collection))
         
         # Print the list of existing databases after attempting to create the database
         print("Existing databases after creating '{}':".format(db_name), mongo_client.list_database_names())
 
-
-    def make_collection(self,db_name,collection_name):
+    def show_data(self):
         """
-        make the colcection in the database
+        Show the data in the collection
         """
+        if self.collection is not None:
+            # Retrieve all documents in the collection
+            documents = self.collection.find()
 
-        #make the collection in the datbase
+            # Print each document
+            for document in documents:
+                print(document)
+        else:
+            print("No collection available. Please create a collection first.")
 
-        pass
 
 
 
 
 
+
+
+
+
+# Create an instance of the Helper_fun class
 helper = Helper_fun()
-helper.make_database("artiles")
+
+# Make the database and collection
+helper.make_database_and_collection("articles", "projects")
+
+# Show the data in the collection
+helper.show_data()
