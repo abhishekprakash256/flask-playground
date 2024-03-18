121c23b6ba9adf7a0e5d043c3a9524a46b754761
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-17 23:17:12 -0400

    mongo code passed

--- testing/test14/mongo/mongo_helper.py
+++ testing/test14/mongo/mongo_helper.py
@@ -193,19 +193,12 @@ class Helper_fun():
 # Create an instance of the Helper_fun class
 helper = Helper_fun()
 
-
-# Make the database and collection
-
-# Make the database and collection
-
-
 # Make the database and collection
 helper.make_database_and_collection("articles", "projects")
 
 #make the collectios
 for val in collection_lst:
     helper.make_collections(val)
-
 
 # Show the data in the collection
 helper.show_data()
