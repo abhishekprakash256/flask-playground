1f2f2d6830559f285dd60e9c84ee84464d7b00c4
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-19 18:57:30 -0400

    mongo data handling

--- testing/test14/mongo/data_handling.py
+++ testing/test14/mongo/data_handling.py
@@ -1,6 +1,10 @@
 #imports 
+
+#the helper funciton to help the mongo db data insertion 
 from mongo_helper import * 
-from read_data import *
+
+#the read data from the json files
+from read_data_json import *
 
 
 #const files
@@ -9,30 +13,30 @@ db_name = "articles"
 collections = ["projects","tech","life"]
 
 
-page_data_list = read_page_data_from_json(File_PATH)
+if __name__ == "__main__":
+    #read the data from the json file 
+    page_data_list = read_page_data_from_json(File_PATH)
 
+    # Create an instance of the Helper_fun class
+    helper = Helper_fun()
 
-# Create an instance of the Helper_fun class
-helper = Helper_fun()
+    # Make the database and collection
+    helper.make_database_and_collection(db_name, collections[0])
 
+    #delete the dummy data from 1st collection
+    helper.delete_data(db_name,collections[0],{'dummy_data': True})
 
-# Make the database and collection
-helper.make_database_and_collection(db_name, collections[0])
+    #make the collections 
+    helper.make_collections(db_name,collection_lst[1])
+    helper.make_collections(db_name,collection_lst[2])
 
-#delete the dummy data from 1st collection
-helper.delete_data(db_name,collections[0],{'dummy_data': True})
+    #show the collections
+    helper.show_collections(db_name)
 
-#make the collections 
-helper.make_collections(db_name,collection_lst[1])
-helper.make_collections(db_name,collection_lst[2])
+    #insert the data in collection 0 
+    helper.insert_data(db_name,collections[0],page_data_list)
 
-#show the collections
-helper.show_collections(db_name)
-
-#insert the data in collection 0 
-helper.insert_data(db_name,collections[0],page_data_list)
-
-#show the data 
-#helper.show_data(db_name,collections[0])
-#article_data = helper.show_article_data(db_name,collections[0],{'article_name': 'patching-unpatching'})
+    #show the data 
+    #helper.show_data(db_name,collections[0])
+    #article_data = helper.show_article_data(db_name,collections[0],{'article_name': 'patching-unpatching'})
 
