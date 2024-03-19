4dda2aa7bdb4c60e8cfe7c0b98b6cfc738ecba83
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-19 07:29:29 -0400

    data insertion succesfull

--- testing/test14/app.py
+++ testing/test14/app.py
@@ -41,30 +41,16 @@ def project():
 def article(article_name):
 
     page_data2 = data_insertion.helper.show_article_data(db_name,collections[0],{'article_name': 'patching-unpatching'})
-    
-    page_data = {
-        'article_name': 'patching-unpatching',
-        'titles': [
-            'First Heading',
-            'Second Heading',
-            'Third Heading'
-        ],
-
-        'image_src': '..\static\images\projects\patching-unpatching\patching.png',
-        'article_paras': [
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        ],
-        'card_one_text': "Some quick example text to build on the card title and make up the bulk of the card's content",
-        'image_url_card_one': "../static/images/misc/cards.jpg",
-        'card_two_text': "Some quick example text to build on the card title and make up the bulk of the card's content",
-        'image_url_card_two': "../static/images/misc/cards.jpg",
-        'card_three_text': "Some quick example text to build on the card title and make up the bulk of the card's content",
-        'image_url_card_three': "../static/images/misc/cards.jpg"
-
-    }
 
     return render_template('projects/patching-unpatching/patching-unpatching.html', **page_data2)
+
+
+@app.route('/test/<article_name>')
+def article_test(article_name):
+
+    page_data = data_insertion.helper.show_article_data(db_name,collections[0],{'article_name': 'another-article'})
+
+    return render_template('projects/patching-unpatching/patching-unpatching.html', **page_data)  
 
 
 
