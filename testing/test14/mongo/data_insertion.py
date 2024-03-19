6d6365e8fa12dea5f0c87b60453cf32fdfd2e5ba
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-18 22:52:43 -0400

    mongo code

--- testing/test14/mongo/data_insertion.py
+++ testing/test14/mongo/data_insertion.py
@@ -16,20 +16,23 @@ helper = Helper_fun()
 
 # Make the database and collection
 helper.make_database_and_collection(db_name, collections[0])
-helper.make_database_and_collection(db_name, collections[1])
-helper.make_database_and_collection(db_name, collections[2])
 
 #delete the dummy data from 1st collection
 helper.delete_data(db_name,collections[0],{'dummy_data': True})
 
+#make the collections 
+helper.make_collections(db_name,collection_lst[1])
+helper.make_collections(db_name,collection_lst[2])
 
 #show the collections
 helper.show_collections(db_name)
 
-#insert the data 
+#insert the data in collection 0 
 helper.insert_data(db_name,collections[0],page_data_list)
 
 #show the data 
 helper.show_data(db_name,collections[0])
+
+#the data insertion is working and tested 
 
 
