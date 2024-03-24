479cd7b4596e87ba050e4c903862c13795af821b
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-24 08:16:30 -0400

    code

--- testing/test16/exp.py
+++ testing/test16/exp.py
@@ -1,36 +1,36 @@
 def dummy():
+
     articles_json = {
-        "articles_data": [
-            {
-            "title": "Article 1 Title",
-            "image": "image1.jpg",
-            "article_para": "Article 1 Paragraph",
-            "markdown_data": "Article 1 Markdown"
-            },
-            {
-            "title": "Article 2 Title",
-            "image": "image2.jpg",
-            "article_para": "Article 2 Paragraph",
-            "markdown_data": "Article 2 Markdown"
-            }
-            
+        "article_name": "patching-unpatching",
+        "aticle_data": [
+            {"title": "Article 1 Title", "image": "", "article_para": "", "markdown_data": "Article 1 Markdown"},
+            {"title": "Article 2 Title", "image": "image2.jpg", "article_para": "Article 2 Paragraph", "markdown_data": "Article 2 Markdown"},
         ],
-        "card_data": {
-            "card_one_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
-            "image_url_card_one": "..\\static\\images\\misc\\cards.jpg",
-            "card_two_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
-            "image_url_card_two": "..\\static\\images\\misc\\cards.jpg",
-            "card_three_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
-            "image_url_card_three": "..\\static\\images\\misc\\cards.jpg"
-        }
-        }
+        "card_one_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+        "image_url_card_one": "..\\static\\images\\misc\\cards.jpg",
+        "card_two_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+        "image_url_card_two": "..\\static\\images\\misc\\cards.jpg",
+        "card_three_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+        "image_url_card_three": "..\\static\\images\\misc\\cards.jpg"
+    }
 
-    page_data = {"articles_data": articles_json}  # Assuming there are other data in page_data
-
-    if page_data is None:
-        return "Page data is missing or invalid."
+    #page_data = {"articles_data": articles_json}  # Assuming there are other data in page_data
     
-    return page_data
+    # Accessing article data
+    article_data = articles_json["aticle_data"]
+    for article in article_data:
+        title = article["title"]
+        image = article["image"]
+        article_para = article["article_para"]
+        markdown_data = article["markdown_data"]
+        print("Title:", title)
+        print("Image:", image)
+        print("Article Paragraph:", article_para)
+        print("Markdown Data:", markdown_data)
+
+    print(articles_json['article_name']) 
+    print(articles_json['card_one_text'])
+    
 
 
 print(dummy())
