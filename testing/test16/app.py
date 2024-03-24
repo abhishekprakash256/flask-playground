1a96c0f495a55ef57b4b33496b300700dd648c1b
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-23 22:43:29 -0400

    Code

--- testing/test16/app.py
+++ testing/test16/app.py
@@ -48,13 +48,35 @@ def section(section_name):
 
 @app.route('/dummy')
 def dummy():
-    articles_json = [
-        {"title": "Article 1 Title", "image": "image1.jpg", "article_para": "", "markdown_data": "Article 1 Markdown"},
-        {"title": "Article 2 Title", "image": "image2.jpg", "article_para": "Article 2 Paragraph", "markdown_data": "Article 2 Markdown"}
-        # Add more articles as needed
-    ]
+    articles_json = {
+        "articles_data": [
+            {
+            "title": "Article 1 Title",
+            "image": "image1.jpg",
+            "article_para": "Article 1 Paragraph",
+            "markdown_data": "Article 1 Markdown"
+            },
+            {
+            "title": "Article 2 Title",
+            "image": "image2.jpg",
+            "article_para": "Article 2 Paragraph",
+            "markdown_data": "Article 2 Markdown"
+            }
+            
+        ],
+        "card_data": {
+            "card_one_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+            "image_url_card_one": "..\\static\\images\\misc\\cards.jpg",
+            "card_two_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+            "image_url_card_two": "..\\static\\images\\misc\\cards.jpg",
+            "card_three_text": "Some quick example text to build on the card title and make up the bulk of the card's content",
+            "image_url_card_three": "..\\static\\images\\misc\\cards.jpg"
+        }
+        }
 
     page_data = {"articles_data": articles_json}  # Assuming there are other data in page_data
+
+    print(page_data)
 
     if page_data is None:
         return "Page data is missing or invalid."
