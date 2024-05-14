6f26b9ef8af85e97649f31d805478603f6a80041
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-05-14 02:22:57 -0400

    code

--- testing/test16/exp.py
+++ testing/test16/exp.py
@@ -2,3 +2,8 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+#r.hset('test-hash','name', 'Alice')
+#r.hset('test-hash','name2','Tom')
+
+print(r.hget('test-hash','name').decode())
+print(r.hget('test-hash','name2').decode())
