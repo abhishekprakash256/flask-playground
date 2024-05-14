e2aea68b1fb7627ba1392e6471a8a4ef07dffd9b
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-05-14 02:24:32 -0400

    code

--- testing/test16/exp.py
+++ testing/test16/exp.py
@@ -5,5 +5,7 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 #r.hset('test-hash','name', 'Alice')
 #r.hset('test-hash','name2','Tom')
 
-print(r.hget('test-hash','name').decode())
-print(r.hget('test-hash','name2').decode())
+r.delete('test-hash')
+
+#print(r.hget('test-hash','name').decode())
+#print(r.hget('test-hash','name2').decode())
